1284c338c609e0b88e611203331ac59e18955931
multipacman <abdulhadhee1@gmail.com>
2022-03-04 04:15:41 +0530

    Added initial routing from splash to login page

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import './services/navigation_services.dart';
 
 // Pages
 import './pages/splash_page.dart';
+import './pages/login_page.dart';
 
 void main() {
   runApp(
@@ -35,7 +36,11 @@ class MainApp extends StatelessWidget {
           backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),
         ),
       ),
-      // navigatorKey: NavigationService.navigatorKey,
+      navigatorKey: NavigationService.navigatorKey,
+      initialRoute: '/login',
+      routes: {
+        '/login': (BuildContext _context) => LoginPage(),
+      },
     );
   }
 }
