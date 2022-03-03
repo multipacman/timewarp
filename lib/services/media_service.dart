03c3bdd2ec2b7f962c7bf4720804f56ff41dcb9f
multipacman <abdulhadhee1@gmail.com>
2022-03-04 03:48:32 +0530

    Added empty constructor to Filepicker service

--- lib/services/media_service.dart
+++ lib/services/media_service.dart
@@ -1,6 +1,8 @@
 import 'package:file_picker/file_picker.dart';
 
 class MediaService {
+  MediaService() {}
+
   Future<PlatformFile?> pickImageFromLibrary() async {
     FilePickerResult? _result =
         await FilePicker.platform.pickFiles(type: FileType.image);
